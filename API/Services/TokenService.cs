e6df5087c9ce76b3960c3b44a47d49d25583b944
César Eduardo Elias <RBcesar484807@gmail.com>
2025-09-12 07:40:03 -0600

    parcial02 - Add Token Service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using API.Intefaces;
+using API.Entitites;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
