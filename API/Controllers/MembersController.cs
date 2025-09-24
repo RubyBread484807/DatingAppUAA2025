bda600f42c32cadfd8517257e3915b088e1cfd03
César Eduardo Elias <RBcesar484807@gmail.com>
2025-09-23 21:10:09 -0600

    parcial02 - Add JWT Middleware

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,21 +1,23 @@
 using API.Data;
 using API.Entitites;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+[Authorize]
 public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
         var members = await context.Users.ToListAsync();
-        
+
         return members;
     }
 
-
+    [AllowAnonymous]
     [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
--- API/DTOs/UserResponse.cs
+++ API/DTOs/UserResponse.cs
@@ -1,4 +1,5 @@
-using System;
+using API.Entitites;
+using API.Intefaces;
 
 namespace API.DTOs;
 
