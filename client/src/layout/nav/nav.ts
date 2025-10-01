ee0dab332c0d2fea1e9de139ea213813878cdbb9
César Eduardo Elias <RBcesar484807@gmail.com>
2025-10-01 07:47:39 -0600

    parcial02 - Add nav links

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,6 +10,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './nav.css'
 })
 export class Nav {
+  private router = inject(Router);
   protected accountService = inject(AccountService);
   protected creds: any = {};
 
@@ -17,6 +18,7 @@ export class Nav {
     this.accountService.login(this.creds).subscribe({
       next: response => {
         console.log(response);
+        this.router.navigateByUrl('/members');
         this.creds = [];
       },
       error: error => alert(error.message)
@@ -25,5 +27,6 @@ export class Nav {
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
