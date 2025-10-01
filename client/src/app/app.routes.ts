f3b25dc1f6934ddfcb0f5d9c19bff332c1f9bbf6
César Eduardo Elias <RBcesar484807@gmail.com>
2025-10-01 07:22:54 -0600

    parcial02 - Add Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MemberList } from '../features/members/member-list/member-list';
+import { Home } from '../features/home/home';
+import { MemberDetail } from '../features/members/member-detail/member-detail';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Home}, //ruta raiz
+    { path: "members", component: MemberList },
+    { path: "members/(id)", component: MemberDetail},
+    { path: "lists", component: Lists},
+    { path: "messages", component: Messages},
+    { path: "**", component: Home} // ruta vacio cuando no hace "match" con ninguna de las anteriores
+];
