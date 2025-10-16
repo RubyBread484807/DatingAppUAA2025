5815aed9ebed93186954e00f60e5fca4884051c5
César Eduardo Elias <RBcesar484807@gmail.com>
2025-10-16 00:08:59 -0600

    parcial02 - Add Member Migration

---
+++ API/Entitites/Photo.cs
@@ -0,0 +1,11 @@
+namespace API.Entitites;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public string? PublicId { get; set; }
+    
+    // Navigation properties
+    public Member Member { get; set; } = null!;
+}
