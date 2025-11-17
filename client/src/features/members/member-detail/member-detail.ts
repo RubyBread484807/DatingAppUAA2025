5c032c53062d565de699228c3c68d9b4a5ab0527
César Eduardo Elias <RBcesar484807@gmail.com>
2025-11-17 14:10:46 -0600

    parcial03 - Add member detail child components

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
