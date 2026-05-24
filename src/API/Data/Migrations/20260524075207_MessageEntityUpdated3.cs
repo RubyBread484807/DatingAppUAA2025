88d62f6e8df1bc9c43aaff6bca0b757a61020b3d
César Eduardo Elias <RBcesar484807@gmail.com>
2026-05-24 02:29:04 -0600

    parcial06 - SignalR - Update migrations

---
+++ src/API/Data/Migrations/20260524075207_MessageEntityUpdated3.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageEntityUpdated3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "4ce0edbd-e3a4-49be-9572-58d6b23da8c7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "7a6818dc-6840-4deb-ad4d-dbebf52f6ddd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "4fe9abb1-d7c7-46fb-89a7-921c901240dd");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "5e075b5f-58a9-48e0-a895-2168ec930748");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "2d85aff6-f1ef-4e83-9d21-c6a0989e497d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "940ab2fe-e28e-46ac-afa8-66321d56a4b8");
+        }
+    }
+}
