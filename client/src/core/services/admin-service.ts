3e65e5367b4a7cf8f0a516cb5cb675f93083bfb8
César Eduardo Elias <RBcesar484807@gmail.com>
2026-05-24 00:51:19 -0600

    parcial06 - IDENTITY - Add edit roles component

--- client/src/core/services/admin-service.ts
+++ client/src/core/services/admin-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../../types/user';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  getUserWithRoles() {
+    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+  }
 }
