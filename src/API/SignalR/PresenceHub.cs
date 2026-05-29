79466fe7a2411e10d7a565b3bdc42ae51d747c49
César Eduardo Elias <RBcesar484807@gmail.com>
2026-05-28 20:51:58 -0600

    parcial06 - SignalR - Fix to GetUserID

--- src/API/SignalR/PresenceHub.cs
+++ src/API/SignalR/PresenceHub.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -25,7 +25,7 @@ public class PresenceHub(PresenceTracker presenceTracker) : Hub
 
     private string GetUserId()
     {
-        return Context.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new HubException("Cannot get member id");
+        return Context.User?.GetMemberId() ?? throw new HubException("Cannot get member id");
     }
 
     private async Task GetOnlineUsers()
